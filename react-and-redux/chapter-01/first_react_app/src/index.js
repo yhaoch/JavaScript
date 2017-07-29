c9cf2abb86dafcb1e7c4f9a4d25b3d5c251f8011
Haochuan Yang <u6001301@anu.edu.au>
2017-07-29 20:03:10 +1000

    changed chapter-03

--- react-and-redux/chapter-01/first_react_app/src/index.js
+++ react-and-redux/chapter-01/first_react_app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ClickCounter from './ClickCounter';
+import FetchDemo from './fetch_test';
 import './index.css';
 
 ReactDOM.render(
-  <ClickCounter/>,
+  <FetchDemo/>,
   document.getElementById('root')
 );
