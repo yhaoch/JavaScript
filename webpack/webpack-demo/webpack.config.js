3cb22eb649acd31fcfc3969ac9b6a34a7c0420c0
Haochuan Yang <u6001301@anu.edu.au>
2018-01-03 22:32:23 +0800

    add something

---
+++ webpack/webpack-demo/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/script/main.js',
+    output: {
+        path: path.resolve(__dirname,'./dist/js'),
+        filename: 'bundle.js'
+    }
+}
